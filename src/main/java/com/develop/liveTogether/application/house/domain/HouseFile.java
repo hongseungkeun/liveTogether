1ca4b0e3fc24996265153fb42de3cce53efcc9be
hongseungkeun <eiziong1@gmail.com>
2023-03-13 22:34:30 +0900

    refactor: 방 파일 entity 리펙토링

---
+++ src/main/java/com/develop/liveTogether/application/house/domain/HouseFile.java
@@ -0,0 +1,29 @@
+package com.develop.liveTogether.application.house.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class HouseFile {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long houseFileNumber;
+	private String houseFileName;
+	private String houseFileNameOriginal;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "houseNumber")
+	private House house;
+
+	public HouseFile(Long houseFileNumber, String houseFileName,
+					 String houseFileNameOriginal, House house)
+	{
+		this.houseFileNumber = houseFileNumber;
+		this.houseFileName = houseFileName;
+		this.houseFileNameOriginal = houseFileNameOriginal;
+		this.house = house;
+	}
+}
