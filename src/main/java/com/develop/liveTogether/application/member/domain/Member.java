8857dd4d09c278922891e46b4d0575800c729c81
hongseungkeun <eiziong1@gmail.com>
2023-03-13 22:32:41 +0900

    refactor: 회원 entity 리펙토링

--- src/main/java/com/develop/liveTogether/application/member/domain/Member.java
+++ src/main/java/com/develop/liveTogether/application/member/domain/Member.java
@@ -1,22 +1,22 @@
-package com.develop.liveTogether.app.member.entity;
+package com.develop.liveTogether.application.member.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
-	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
-	private Long memberNumber;
 	@Column(nullable = false)
 	private String memberId;
 	@Column(nullable = false)
 	private String memberPw;
 	@Column(nullable = false)
 	private String memberName;
-	@Column(nullable = false)
+	@Column(unique = true, nullable = false)
 	private String memberNickname;
 	@Column(nullable = false)
 	private String memberPhone;
@@ -25,20 +25,14 @@ public class Member {
 	@Column(nullable = false)
 	private String memberProfile;
 	@Column(nullable = false)
-	private String memberType;
-	
-	protected Member(){
-
-	}
+	private String memberRole;
 
 	@Builder
-	public Member(Long memberNumber, String memberId,
-				  String memberPw, String memberName,
-				  String memberNickname, String memberPhone,
-				  String memberGender, String memberProfile,
-				  String memberType)
+	public Member(String memberId, String memberPw,
+				  String memberName, String memberNickname,
+				  String memberPhone, String memberGender,
+				  String memberProfile, String memberRole)
 	{
-		this.memberNumber = memberNumber;
 		this.memberId = memberId;
 		this.memberPw = memberPw;
 		this.memberName = memberName;
@@ -46,6 +40,6 @@ public class Member {
 		this.memberPhone = memberPhone;
 		this.memberGender = memberGender;
 		this.memberProfile = memberProfile;
-		this.memberType = memberType;
+		this.memberRole = memberRole;
 	}
 }
