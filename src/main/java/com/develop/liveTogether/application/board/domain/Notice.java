636a6abf24f10d4044a9c36e0fc31735d63e754b
hongseungkeun <eiziong1@gmail.com>
2023-03-13 23:08:36 +0900

    refactor: 네이밍 notice로 변경

---
+++ src/main/java/com/develop/liveTogether/application/board/domain/Notice.java
@@ -0,0 +1,45 @@
+package com.develop.liveTogether.application.board.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Notice {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long noticeNumber;
+	@Column(nullable = false)
+	private String noticeTitle;
+	@Column(nullable = false)
+	private String noticeContent;
+	@CreatedDate
+	@Column(nullable = false)
+	private String noticeCreateDate;
+
+	@Builder
+	public Notice(Long noticeNumber, String noticeTitle,
+				  String noticeContent, String noticeCreateDate)
+	{
+		this.noticeNumber = noticeNumber;
+		this.noticeTitle = noticeTitle;
+		this.noticeContent = noticeContent;
+		this.noticeCreateDate = noticeCreateDate;
+	}
+
+	@PrePersist
+	public void onPrePersist(){
+		this.noticeCreateDate = LocalDateTime.now()
+				.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+	}
+}
