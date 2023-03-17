fa467e4abf83ca1124fa64f77c2e08f4eb0dce9b
hongseungkeun <eiziong1@gmail.com>
2023-03-17 15:27:14 +0900

    add: 비밀번호 변경 추가

--- src/main/java/com/develop/liveTogether/application/member/controller/MemberController.java
+++ src/main/java/com/develop/liveTogether/application/member/controller/MemberController.java
@@ -2,6 +2,9 @@ package com.develop.liveTogether.application.member.controller;
 
 import com.develop.liveTogether.application.member.dto.request.*;
 import com.develop.liveTogether.application.member.service.MemberService;
+import com.develop.liveTogether.global.util.SessionUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,14 +30,18 @@ public class MemberController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Void> login(@Valid @RequestBody LoginRequest request){
-        memberService.login(request);
+    public ResponseEntity<Void> login(@Valid @RequestBody LoginRequest request, HttpServletRequest httpServletRequest){
+        String memberId = memberService.login(request);
+
+        SessionUtil.createSession(memberId, httpServletRequest);
 
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/logout")
-    public ResponseEntity<Void> logout(){
+    public ResponseEntity<Void> logout(HttpSession session){
+        SessionUtil.removeSession(session);
+
         return ResponseEntity.ok().build();
     }
 
@@ -66,4 +73,10 @@ public class MemberController {
         return ResponseEntity.ok().build();
     }
 
+    @PostMapping("/find/changePw")
+    public ResponseEntity<String> findPw(@Valid @RequestBody ChangePwRequest request){
+        memberService.changePw(request);
+
+        return ResponseEntity.ok().build();
+    }
 }
