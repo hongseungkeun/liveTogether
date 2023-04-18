0455b86cbb1aca89e5b92003f5b586ca022b1f49
hongseungkeun <eiziong1@gmail.com>
2023-04-18 21:32:00 +0900

    refactor: 승인된 하우스 리스트를 가져오도록 메소드 수정

---
+++ src/main/java/com/develop/liveTogether/application/house/repository/HouseCustomRepositoryImpl.java
@@ -0,0 +1,40 @@
+package com.develop.liveTogether.application.house.repository;
+
+import com.develop.liveTogether.application.house.domain.House;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+
+import java.util.List;
+
+import static com.develop.liveTogether.application.house.domain.QHouse.house;
+import static com.develop.liveTogether.application.member.domain.QRoomGuest.roomGuest;
+
+public class HouseCustomRepositoryImpl implements  HouseCustomRepository{
+    private final JPAQueryFactory queryFactory;
+
+    public HouseCustomRepositoryImpl(JPAQueryFactory queryFactory) {
+        this.queryFactory = queryFactory;
+    }
+
+    @Override
+    public Slice<House> findByHouseApprovalAndRoomGuestApproval(Pageable pageable) {
+        List<House> lists = queryFactory.selectFrom(house)
+                .where(house.approval.eq(true))
+                .leftJoin(house.roomGuests, roomGuest)
+                .on(roomGuest.approval.eq(3))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        Long count = queryFactory.select(house.count())
+                .from(house)
+                .where(house.approval.eq(true))
+                .leftJoin(house.roomGuests, roomGuest)
+                .on(roomGuest.approval.eq(3))
+                .fetchOne();
+
+        return new PageImpl<>(lists, pageable, count);
+    }
+}
