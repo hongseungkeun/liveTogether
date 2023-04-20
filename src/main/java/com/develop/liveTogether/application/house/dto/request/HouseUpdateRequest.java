3506dc123035a1e47bf8fef4f95115846b3b92d3
hongseungkeun <eiziong1@gmail.com>
2023-04-20 23:10:09 +0900

    refactor: 수정된 내용만 dto에 담도록 수정

--- src/main/java/com/develop/liveTogether/application/house/dto/request/HouseUpdateRequest.java
+++ src/main/java/com/develop/liveTogether/application/house/dto/request/HouseUpdateRequest.java
@@ -25,7 +25,6 @@ public record HouseUpdateRequest(
                 .houseType(this.houseType)
                 .houseGender(matchHouseGender())
                 .houseFixPeopleNum(getHouseFixPeopleNum())
-                .status(Status.WAIT_UPDATE)
                 .address(Address.builder()
                         .houseAddress(this.houseAddress)
                         .houseAddressDetail(this.houseAddressDetail)
