ffc4bfcef911ef5fc0347cac1cc5e47b303f4d01
hongseungkeun <eiziong1@gmail.com>
2023-04-23 21:46:44 +0900

    refactor: @Notnull 추가

--- src/main/java/com/develop/liveTogether/application/house/dto/request/HouseAdminChangeStatusRequest.java
+++ src/main/java/com/develop/liveTogether/application/house/dto/request/HouseAdminChangeStatusRequest.java
@@ -1,8 +1,9 @@
 package com.develop.liveTogether.application.house.dto.request;
 
 import com.develop.liveTogether.application.house.data.Status;
+import jakarta.validation.constraints.NotNull;
 
 public record HouseAdminChangeStatusRequest(
-        Status status
+        @NotNull Status status
 ) {
 }
